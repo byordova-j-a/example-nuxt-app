74c186282680f3d71ba0f03e5eda571f7b4c1f37
byordova-j-a <berd.rabbit@gmail.com>
2025-02-16 21:32:02 +0500

    feat(app): add build

--- middleware/routeMiddleware.ts
+++
@@ -1,7 +0,0 @@
-export default defineNuxtRouteMiddleware((to) => {
-  if (to.path === '/' &&
-    (Object.keys(to.query).length > 1 || (Object.keys(to.query).length && to.query['book-page'] !== '3' && to.query['book-page'] !== '1'))) {
-    return navigateTo('/');
-  }
-
-});
